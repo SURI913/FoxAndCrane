0ac90ef5a36ee75d0219a7759f0fdb8d19c976af
hayum0525 <khyoung9128@gmail.com>
2024-12-13 17:32:52 +0900

    npc , crow
    npc , 까마귀 behaviour

--- Assets/02.Script/branch.cs
+++ Assets/02.Script/branch.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class Branch : MonoBehaviour
 {
-    private Rigidbody rb; // Rigidbody 컴포넌트 저장
+    private Rigidbody rb;
+    private ObstacleManager obstacleManager;
+
 
     void Start()
     {
-        // Rigidbody 초기화
+        
         rb = GetComponent<Rigidbody>();
+
+        obstacleManager = FindObjectOfType<ObstacleManager>();
+        if (obstacleManager == null)
+        {
+            Debug.LogError("is not null");
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        // Ground나 다른 오브젝트와 충돌했을 때
-        if (collision.gameObject.CompareTag("BridgePart"))
+        
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            // rigidbody를 비활성화해서 물리적인 움직임 멈춤
+            //rigidbody를 비활성화해서 물리적인 움직임 멈춤
             if (rb != null)
             {
                 rb.isKinematic = true;
@@ -25,8 +33,23 @@ public class Branch : MonoBehaviour
             //오브젝트 태그 변경
             gameObject.tag = "BridgePart";
 
-            // 충돌된 오브젝트의 부모로 설정하여 이어붙이기
+            //충돌된 오브젝트의 부모로 설정하여 이어붙이기
             this.transform.parent = collision.transform;
         }
     }
+    private IEnumerator ReturnToPoolAfterDelay()
+    {
+        yield return new WaitForSeconds(5f); 
+
+        if (obstacleManager != null)
+        {
+            // ObstacleManager통해 풀로 반환
+            obstacleManager.ReturnObstacle("Branch", this.gameObject);
+        }
+        else
+        {
+            Debug.LogError("");
+        }
+    }
+
 }
